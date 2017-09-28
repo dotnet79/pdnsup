6c3224c4e78013ba953dcb4d5997034713935b01
DoT <antal@lovasi.eu>
2017-09-28 11:46:28 +0200

    1506591988 commit

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
-FILE1=powerdns-update.sh
+FILE[1]=powerdns-update.sh
+FILE[2]=id_rsa
 
 mkdir -p /usr/local/tools/
-wget --no-check-certificate https://raw.githubusercontent.com/dotnet79/pdnsup/master/pdns/$FILE1 -O /usr/local/tools/$FILE1
-chmod +x /usr/local/tools/$FILE1
+
+for i in ${FILE[@]}
+    do
+        echo $i
+        wget --no-check-certificate https://raw.githubusercontent.com/dotnet79/pdnsup/master/pdns/$i -O /usr/local/tools/$i
+        chmod +x /usr/local/tools/$i
+done
+
+chmod 600 /usr/local/tools/id_rsa
 
 if [ `grep "powerdns-update.sh" /var/spool/cron/crontabs/root |wc -l` -eq 0 ]; then
         echo "*/15 * * * * /usr/local/tools/powerdns-update.sh" >>/var/spool/cron/crontabs/root
