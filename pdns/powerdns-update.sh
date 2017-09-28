df8b89ce265b74895573be845d1faa80c6d79fca
DoT <antal@lovasi.eu>
2017-09-28 11:51:24 +0200

    1506592284 commit

--- pdns/powerdns-update.sh
+++ pdns/powerdns-update.sh
@@ -8,6 +8,6 @@ USER=pdnsup
 PASS=d386c800769d27c37bb17b652a4b06d5acd2e57a
 
 #ssh -p22284 pdnsup@szaszsuli.hu "/home/pdnsup/powerdns-dynamic.sh $IP $DOMAIN"
-ssh -p22284 $USER@$DOMAIN "$IP $FDOMAIN" #szerveren az authorized_keys -ben fel van veve command="./pdns/powerdns-dynamic.sh $SSH_ORIGINAL_COMMAND"
+ssh -i /usr/local/tools/id_rsa -p22284 $USER@$DOMAIN "$IP $FDOMAIN" #szerveren az authorized_keys -ben fel van veve command="./pdns/powerdns-dynamic.sh $SSH_ORIGINAL_COMMAND"
 
 exit 0
